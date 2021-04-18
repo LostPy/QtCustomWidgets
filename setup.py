97573d7ec5cc657355e715f44c6d8edfc37c1ee5
LostPy <lostmusic.osu@gmail.com>
2021-04-18 16:59:19 +0200

    20210418.4
    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+# Project: OsuData
+# Author: LostPy
+
+from setuptools import setup, find_packages
+
+import osuData
+
+__doc__ = """My Custom Widgets for PyQt5"""
+
+
+setup(
+	name='osuData',
+	version='1.20210418',
+	author='LostPy',
+	description="My Custom Widgets for PyQt5",
+	long_description=__doc__,
+    package_dir = {'QtCustomWidgets': './QtCustomWidgets'},
+    package_data = {'': ['README.md']},
+	include_package_data=True,
+	url='',
+	classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: Functionnal - improvement in progress",
+        "License :: MIT",
+        "Natural Language :: French",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.5+",
+        "Topic :: PyQt5",
+    ],
+    license='MIT',
+    packages = find_packages()
+    )
